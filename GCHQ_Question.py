c34b8f6a75a4b41b0b63081391a9510b5a1633ff
Brad Roberts <bradley.roberts89@gmail.com>
2018-02-28 09:45:03 +0000

    Update GCHQ_Question.py

--- GCHQ_Question.py
+++ GCHQ_Question.py
@@ -1,6 +1,7 @@
 """Attempt at a variation of a GCHQ Question: 
-What is the longest word in the english dictionary
-which is in ascending alphabetical order but without any alphabetically consecutive letters e.g. ab"""
+Find the longest word in the english dictionary
+which is in ascending alphabetical order but without 
+any alphabetically consecutive letters e.g. ab """
 
 
 import enchant
@@ -11,13 +12,12 @@ alphabet = list(string.ascii_lowercase)
 
 
 def word_builder(word, length=5):
+    """recursive function that builds a word using random non-consecutive letters"""
     alpha = alphabet.index(word[-1:]) + 1
     if len(word) > length:
         return word
     elif alpha >20:
         return word + alphabet[alpha + random.randint(0,25 - alpha)]
-    # print(alpha)
-
     else:
         return word_builder(word + alphabet[alpha + random.randint(0,4)])
 
@@ -28,9 +28,7 @@ sizes = range(5,8)
 for prefix in prefixes:
     best_final = ""
     for size in sizes:
-
-        for i in range(10000):
-
+        for i in range(100000):
             garble = word_builder(prefix, length=size)
             if dic.check(garble) == True:
                 if (len(garble) >= len(best_final)):
@@ -38,5 +36,5 @@ for prefix in prefixes:
 
     print(best_final)
 
-
+# the words "begins" and "chimps" are returned!
 
